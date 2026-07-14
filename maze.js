52da6b0a93a94da9fa3eec24955f8d92b08c1587
Daniel Brendel <dbrendel1988@gmail.com>
2026-07-14 22:03:43 +0200

    Maze applet

---
+++ maze.js
@@ -0,0 +1,138 @@
+/**
+ * Maze Applet
+ */
+window.Maze = class {
+    /**
+     * Construct class object instance
+     */
+    constructor()
+    {
+    }
+
+    /**
+     * Called when the applet is installed
+     * 
+     * @return void
+     */
+    onInstall()
+    {
+    }
+
+    /**
+     * Called when the applet is uninstalled
+     * 
+     * @return void
+     */
+    onRemove()
+    {
+    }
+
+    /**
+     * Called when the applet is loaded
+     * This happens everytime the page is loaded/refreshed, or when the applet is installed
+     * 
+     * @return void
+     */
+    onLoad()
+    {
+    }
+
+    /**
+     * Called when the applet is shown, e.g. when launching via desktop
+     * 
+     * @return void
+     */
+    onShow()
+    {
+		document.getElementById('maze-applet').innerHTML = `<iframe 
+			scrolling="no"
+			src="https://maze3d.danielbrendel.com/maze.html">
+		</iframe>`;
+    }
+
+    /**
+     * Called when the applet is closed, e.g. via the close action button in the title bar
+     * 
+     * @return void
+     */
+    onClose()
+    {
+		document.getElementById('maze-applet').innerHTML = '';
+    }
+
+    /**
+     * Return the HTML content which is rendered into the applet window
+     * 
+     * @return string
+     */
+    view()
+    {
+        return `
+            <div id="maze-applet"></div>
+        `;
+    }
+
+    /**
+     * Provide applet settings here
+     * 
+     * @return object
+     */
+    settings()
+    {
+        return {
+            wndWidth: '803px',
+            wndHeight: '627px',
+            btnClose: true,
+            btnMaximize: false,
+            btnMinimize: false
+        };
+    }
+
+    /**
+     * Return basic information on the applet
+     * 
+     * @return object
+     */
+    infos()
+    {
+        return {
+            name: 'Maze',
+            version: '1.0',
+            icon: window.location.origin + '/img/icons/maze.png'
+        };
+    }
+
+    /**
+     * Return the CSS styles which are rendered into the page
+     * 
+     * @returns object
+     */
+    styles()
+    {
+        return `
+			#column-window-maze .window {
+				background-color: #75C8FF;
+			}
+		
+            #column-window-maze .window-body {
+				width: 100%;
+				height: 100%;
+			}
+		
+			#maze-applet {
+			  position: relative;
+			  top: -8px;
+			  left: -8px;
+			  width: 100%;
+			  height: 96.8%;
+			  overflow: hidden;
+			}
+			
+			#maze-applet iframe {
+                width: 100%;
+				height: 100%;
+				border: none;
+            }
+        `;
+    }
+}
